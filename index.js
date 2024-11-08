c625ee9bf783c2a0d145ec4543f5202915060b84
ucangun <149247682+ucangun@users.noreply.github.com>
2024-11-08 21:04:52 +0100

    "Added helmet and rate limiting middlewares, updated error handling and refactored code organization"

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 const mongoSanitize = require("express-mongo-sanitize");
+const helmet = require("helmet");
+const rateLimit = require("express-rate-limit");
 
-// db connection
+// DB connection
 require("./server");
 
 const PORT = process.env.PORT || 8000;
@@ -14,19 +16,36 @@ const PORT = process.env.PORT || 8000;
 
 // Middlewares:
 
+// Middleware for parsing incoming JSON requests
 app.use(express.json());
+
+// Handle async errors in the application using express-async-errors
 require("express-async-errors");
 
+// Rate limiting to prevent too many requests from a single IP
+// This is to protect the API from DDoS or brute force attacks
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Allow only 100 requests per IP in the time window
+  message: "Too many requests from this IP, please try again later.",
+});
+app.use(limiter); // Apply rate limiting to all incoming requests
+
+// Route for Todo-related operations
 app.use("/todo", require("./src/routes/todoRouter"));
+
+// Helmet secures your app by setting various HTTP headers to protect against common vulnerabilities.
+app.use(helmet());
 
 // Sanitize user input to prevent NoSQL injection attacks
 app.use(mongoSanitize());
 
-// catch errors
+// Error handler middleware to catch and respond to errors
 app.use(require("./src/middlewares/errorHandler"));
 
 /********************************************************************/
 
+// Start the server
 app.listen(PORT, () => {
   console.log(`Server running on http://127.0.0.1:${PORT}`);
 });
