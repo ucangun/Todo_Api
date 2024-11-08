21d2c09c6eea1a42a23acc1773ee0232c4781a0f
ucangun <149247682+ucangun@users.noreply.github.com>
2024-11-08 15:05:00 +0100

    "Add Todo controller with list and create methods"

---
+++ src/controllers/todoController.js
@@ -0,0 +1,23 @@
+const Todos = require("../models/todoModel");
+
+module.exports = {
+  list: async (req, res) => {
+    const todos = await Todos.find();
+    res.status(200).send({
+      error: false,
+      message: "Todos retrieved successfully",
+      data: todos,
+    });
+  },
+
+  create: async (req, res) => {
+    const newTodo = await Todos.create({
+      text: req.body.title,
+    });
+    res.status(201).send({
+      error: false,
+      message: "Todo created successfully",
+      data: newTodo,
+    });
+  },
+};
