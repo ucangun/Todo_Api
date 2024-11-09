10bc22e35fe2bc7c3bcc601da94560f612ad0b1f
ucangun <149247682+ucangun@users.noreply.github.com>
2024-11-09 12:57:20 +0100

    "Added connect timeout and socket timeout options to mongoose connection"

--- server.js
+++ server.js
@@ -3,7 +3,10 @@
 const mongoose = require("mongoose");
 
 mongoose
-  .connect(process.env.MONGOURI || "mongodb://localhost:27017/todoAPI")
+  .connect(process.env.MONGOURI || "mongodb://localhost:27017/todoAPI", {
+    connectTimeoutMS: 20000,
+    socketTimeoutMS: 45000,
+  })
   .then(() => console.log("DB connected"))
   .catch((error) => console.log("DB not connected:", error));
 
